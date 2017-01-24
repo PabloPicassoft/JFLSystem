e513edbe5bd82f57fb4a80b48032eb261d3fe8e1
PabloPicassoft <pablo.picassoft@gmail.com>
2017-01-24 17:34:30 +0000

    Reset Database

--- JFLSystem/src/JFLPACK/DBConnect.java
+++ JFLSystem/src/JFLPACK/DBConnect.java
@@ -5,6 +5,9 @@
  */
 package JFLPACK;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author Paul
@@ -12,6 +15,17 @@ package JFLPACK;
 
 public class DBConnect {
     public static void main(String[] args) {
+
+        try {
+            String host = "jdbc:derby://localhost:1527/JFLDB [JFLAdmin on JFLADMIN]";
+            String uName = "JFLAdmin";
+            String uPass= "JFLAdmin";
+            
+            Connection con = DriverManager.getConnection(host, uName, uPass);
+        }
+        catch ( SQLException err ) {
+            System.out.println( err.getMessage( ) );
+        }
         
     }
 }
